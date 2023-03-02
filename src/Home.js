2b0384d378561faf8f622b92f7e9801454b61dff
Siddhesh <siddhesh.nandurkar@gmail.com>
2023-03-02 17:36:13 +0530

    name change

--- src/Home.js
+++ src/Home.js
@@ -80,7 +80,7 @@ function App() {
           state={{ isNew: true, bookType }}
           className="bg-blue-500 mr-12 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
         >
-          New Item
+          Add New
         </Link>
 
         <button
